39bcf09751482b7301ce33ddfc849f5f7401de15
Lhamap <44235006+Lhamap@users.noreply.github.com>
2018-10-24 15:40:31 +0200

    Update Zone.py

--- Simulation/Zone.py
+++ Simulation/Zone.py
@@ -27,11 +27,14 @@ class Building_zone(Toplevel):
         r,c=0,0    # row and column of widgets in frame_1
         Button(self.frame_1,text='Nettoyer',command=self.clear).grid(row=r,column=c)
         r+=1
+        self.coor_label = Label(self.frame_1,text='coordonnées :\n(0,0)')
+        self.coor_label.grid(row=r,column=c,rowspan=2)
         self.frame_1.grid(row=0,column=1,sticky=N)
         
         # Binding
         self.canvas.bind('<B1-Motion>',self.clic)  # Allow to add walls by maintaining clic
         self.canvas.bind('<Button-1>',self.clic)   # Allow to add a wall with clic
+        self.canvas.bind('<Button-3>',self.coordinates)
         self.bind('<Escape>',self.leave)
         
     def clic(self,event):
@@ -42,8 +45,14 @@ class Building_zone(Toplevel):
         self.master.entities.grid[nx][ny].set_wall(wall)
         wall.plot(self.canvas)
         
+    def coordinates(self,event):
+        x,y=event.x,event.y
+        (nx,ny)=self.master.entities.which_cell(x,y)
+        self.coor_label.config(text='coordonnées :\n('+str(nx)+','+str(ny)+')')
+        
     def clear(self):
         self.master.entities.reset_grid()
+        self.master.entities.reset_walls()
         self.canvas.delete('all')
         
     def leave(self,event):
