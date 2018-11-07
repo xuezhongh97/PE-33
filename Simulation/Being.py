6f9a77f90d1220d20b13b7b6db243969bc31095a
Dhanjoor <44234827+Dhanjoor@users.noreply.github.com>
2018-11-07 15:21:01 +0100

    Add files via upload

--- Simulation/Being.py
+++ Simulation/Being.py
@@ -6,13 +6,20 @@ Created on Wed Oct 24 14:38:58 2018
 """
 
 from tkinter import *
+from Entities import *
+import numpy as np
 
 class Being:
-    def __init__(self,master,pixposition,case,speed):
+    def __init__(self,master,pixposition,cell,speed):
         self.master=master
         self.pixposition=pixposition      # (x,y) for position in pixels
-        self.case=case                   # The case the being is in
+        self.cell=cell                  # The cell the being is in
         self.speed=speed                # (vr,vteta) vr is the norm and vteta the angle of the speed
+    
+    def move(self,t):
+        self.pixposition[0]+=t*self.speed[0]*np.cos(self.speed[1])                  #new position of the being
+        self.pixposition[1]+=t*self.speed[0]*np.sin(self.speed[1])
+        self.cell=self.master.which_cell(self.pixposition[0],self.pixposition[1])
         
 class Zombie(Being):
     def __init__(self,master,pixposition,case,speed,lifespan):
