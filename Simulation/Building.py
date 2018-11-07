bf9cc3a6a03db6a760dbf4bfd504aca18602c576
Lhamap <44235006+Lhamap@users.noreply.github.com>
2018-11-07 16:43:26 +0100

    Add files via upload

--- Simulation/Building.py
+++ Simulation/Building.py
@@ -10,18 +10,25 @@ class Cell:
     def __init__(self,master,x,y,nx,ny,floors):
         self.master=master     # the entities object
         self.center=[x,y]     # Position in pixels of the center
-        self.n=[nx,ny]       # Position in the grid
+        self.n=(nx,ny)       # Position in the grid
         self.floors=floors
         self.content=None
         
     def set_wall(self):
         self.content='w'
     
-    def plot(self,zone):                    #permet d'afficher les batiments
+    def plot(self,zone):
         [xc,yc],e=self.center,self.master.ppc
         zone.create_rectangle(xc-e//2,yc-e//2,xc+e//2,yc+e//2,fill='black')
 
 class Building:
-    def __init__(self,master):
+    def __init__(self,master,walls,no,se):
         self.master=master
-        self.walls=[]     # list of cells
+        self.walls=walls     # list of cells' indices
+        self.no=no      # indices (nx,ny) of the cell in the top left corner
+        self.se=se      # indices of the cell in the bottom right corner
+    
+    def plot(self):
+        for n in self.walls:
+            (nx,ny)=n
+            self.master.grid[nx][ny].plot()
--- Simulation/Zone.py
+++ Simulation/Zone.py
@@ -21,8 +21,8 @@ class Building_zone(Toplevel):
         self.master = master
         
         # Visual zone
-        self.canvas = Canvas(self,width=w,height=h)
-        self.canvas.grid(row=0,column=0)
+        self.canvas = Canvas(self,width=w,height=h,bg='gray50')
+        self.canvas.grid(row=0,column=0,rowspan=6)
         for wall in self.master.entities.walls:    # Plot walls that are already saved when oppening a building zone
             wall.plot(self.canvas)
             
@@ -33,25 +33,53 @@ class Building_zone(Toplevel):
         r+=1
         self.coor_label = Label(self.frame_1,text='coordonnées :\n(0,0)')
         self.coor_label.grid(row=r,column=c,rowspan=2)
+        Button(self.frame_1,text='Creer un mur au clic',command=self.setclic_wall)
         self.frame_1.grid(row=0,column=1,sticky=N)
         
+        # New building zone : where we can create a new building
+        self.frame_2 = Frame(self)
+        Label(self.frame_2,text='--------------------------').grid(row=0,column=0,columnspan=2)
+        Label(self.frame_2,text='Bâtiments rectangulaires').grid(row=1,column=0,columnspan=2)
+        Label(self.frame_2,text='largeur :').grid(row=2,column=0)
+        Label(self.frame_2,text='hauteur :').grid(row=3,column=0)
+        Button(self.frame_2,text='Creer un bâtiment au clic',command=self.setclic_building)
+        self.width_entry = Entry(self.frame_2)
+        self.height_entry = Entry(self.frame_2)
+        self.width_entry.grid(row=2,column=1)
+        self.height_entry.grid(row=3,column=1)
+        self.frame_2.grid(row=1,column=1,sticky=N)
+        
         # Binding
-        self.canvas.bind('<B1-Motion>',self.clic)  # Allow to add walls by maintaining clic
-        self.canvas.bind('<Button-1>',self.clic)   # Allow to add a wall with clic
         self.canvas.bind('<Button-3>',self.coordinates)  # Allow to print the coordinates of the point that is right-clicked
         self.bind('<Escape>',self.leave)
         
-    def clic(self,event):
+    def clic_wall(self,event):   # called to create a wall
         x,y=event.x,event.y
         (nx,ny)=self.master.entities.which_cell(x,y)      # The indices of the case that were clicked on
         self.master.entities.walls.append((nx,ny))
         self.master.entities.grid[nx][ny].set_wall()
         self.master.entities.grid[nx][ny].plot(self.canvas)
         
+    def clic_building(self,event):    # called to create a building
+        w,h=int(self.width_entry.get()),int(self.height_entry.get())
+        (nx,ny)=self.master.entities.which_cell(event.x,event.y)
+        no=(nx-w//2,ny-h//2)     # top left cell
+        se=(no[0]+w-1,no[1]+h-1)     # bottom right cell
+        walls=[(nx + k,ny) for k in range(w)]+[(nx+k,ny+h-1) for k in range(w)]+[(nx,ny+k) for k in range(1,h-1)]+[(nx+w-1,ny+k) for k in range(1,h-1)]
+        
+        # A terminer
+        
     def coordinates(self,event):
         x,y=event.x,event.y
         (nx,ny)=self.master.entities.which_cell(x,y)
         self.coor_label.config(text='coordonnées :\n('+str(nx)+','+str(ny)+')')
+        
+    def setclic_building(self):
+        self.canvas.bind('<Button-1>',self.clic_building)   # Allow to add a wall with clic
+        
+    def setclic_wall(self):
+        self.canvas.bind('<B1-Motion>',self.clic_wall)  # Allow to add walls by maintaining clic
+        self.canvas.bind('<Button-1>',self.clic_wall)   # Allow to add a wall with clic
         
     def clear(self):
         self.master.entities.reset_grid()
