ab92270669b294888cc23cf324ae4d29f3565247
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-02-06 14:56:14 +0100

    Add files via upload

--- Simulation/DetectSound.py
+++ Simulation/DetectSound.py
@@ -1,9 +1,12 @@
-def DetectSound(G,x,y,R):
-    L=[0,0]
+from math import atan
+
+def detectSound(G,x,y,R):
+    u,v=0,0
     for i in range(x-R,x+R+1):
         for j in range(y-R,y+R+1):
             r=((i-x)**2+(j-y)**2)**(1/2)
             if r<=R and r!=0:
-                L[0]+=G[i][j]*(i-x)/r
-                L[1]+=G[i][j]*(j-y)/r
-    return(L)
+                volume=G[i][j].sound
+                u+=volume*(i-x)/r
+                v+=volume*(j-y)/r
+    return(2*atan(v/(u+(u**2+v**2)**(1/2))))
