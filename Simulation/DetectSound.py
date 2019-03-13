1412c72b46b2fbd1b13f7ab244932452c3895bc0
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-03-13 17:00:53 +0100

    Add files via upload

--- Simulation/DetectSound.py
+++ Simulation/DetectSound.py
@@ -1,10 +1,23 @@
-def detectSound(G,x,y,R):
+def detectSound(self,G,x,y):
     u,v=0,0
-    for i in range(x-R,x+R+1):
-        for j in range(y-R,y+R+1):
-            r=((i-x)**2+(j-y)**2)**(1/2)
-            if r<=R and r!=0:
-                volume=G[i][j].sound
-                u+=volume*(i-x)/r
-                v+=volume*(j-y)/r
+    if x>0:
+    	u-=G[x-1][y].sound
+    	if y>0:
+    		u-=G[x-1][y-1].sound/2**0.5
+    	if y<yMax:
+    		u-=G[x-1][y+1].sound/2**0.5
+
+    if x<xMax-1:
+    	u+=G[x+1][y].sound
+    	if y>0:
+    		u-=G[x+1][y-1].sound/2**0.5
+    	if y<yMax:
+    		u-=G[x+1][y+1].sound/2**0.5
+
+    if y>0:
+    	v-=G[x][y-1].sound
+    if y<yMax:
+    	v+=G[x][y+1].sound
+
     return(u,v)
+    #pas pris en compte hearing et le son en (x,y) ici
